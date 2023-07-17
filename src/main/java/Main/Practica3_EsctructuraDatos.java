5632316df726fd379ad5086229c51c31f9d893f5
Samantha Suquilanda Quilli <samanthasuquilandaquilli@MacBook-Pro-de-Samantha.local>
2023-07-17 11:10:52 -0500

    Hola mundo

--- src/main/java/Main/Practica3_EsctructuraDatos.java
+++ src/main/java/Main/Practica3_EsctructuraDatos.java
@@ -4,6 +4,9 @@
 
 package Main;
 
+import controlador.ArbolContactos;
+import modelo.Contacto;
+
 /**
  *
  * @author samanthasuquilandaquilli
@@ -11,6 +14,19 @@ package Main;
 public class Practica3_EsctructuraDatos {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ArbolContactos arbol = new ArbolContactos();
+        
+        Contacto c1 = new Contacto("Juan", "1234");
+        Contacto c2 = new Contacto("Maria", "1234");
+        Contacto c3 = new Contacto("Pedro", "1234");
+        Contacto c4 = new Contacto("Pablo", "1234");
+
+        arbol.insertar(c1);
+        arbol.insertar(c2);
+        arbol.insertar(c3);
+        arbol.insertar(c4);
+        arbol.insertar(new Contacto("Julina", "12345"));
+        arbol.insertar(new Contacto("Jhon", "12345"));
+
     }
 }
--- src/main/java/controlador/ArbolContactos.java
+++ src/main/java/controlador/ArbolContactos.java
@@ -4,10 +4,80 @@
  */
 package controlador;
 
+import modelo.Contacto;
+import modelo.Nodo;
+
 /**
  *
  * @author samanthasuquilandaquilli
  */
 public class ArbolContactos {
+    private Nodo raiz;
+
+    public ArbolContactos(Nodo raiz) {
+        this.raiz = null;
+    }
+
+    public ArbolContactos() {
+    }
     
-}
+    
+    public void insertar (Contacto nuevoContacto){
+        if(raiz == null){
+            raiz = new Nodo (nuevoContacto);
+        }else{
+            insertarRecursivo(raiz, nuevoContacto);
+        }
+        
+    }
+    private  void insertarRecursivo(Nodo nodo, Contacto nuevoContacto){
+        if(nuevoContacto.getNombre().compareTo(nodo.getContacto().getNombre()) < 0 ){
+            if(nodo.getLeft()==null){
+                nodo.setLeft(new Nodo(nuevoContacto));
+            }else{
+                insertarRecursivo(nodo.getLeft(),nuevoContacto);
+            }
+        }else if (nuevoContacto.getNombre().compareTo(nodo.getContacto().getNombre()) > 0 ){
+           if(nodo.getRight()==null){
+               nodo.setRight(new Nodo(nuevoContacto));
+           }else{
+               insertarRecursivo(nodo.getRight(),nuevoContacto);
+           }
+        }else{
+            //Si el nombre del contacto nuevo es igual a uno ya existente
+            
+        }
+    }
+    
+    public boolean estaEquilibrado(){
+        return verificarBalance(raiz);
+    }
+    private boolean verificarBalance (Nodo nodo){
+        if(nodo == null){
+            return true; //Si es arbol vacio considero equilibrado
+        }
+        int alturaIzquierda = obtenerAltura(nodo.getLeft());
+        int alturaDerecha = obtenerAltura(nodo.getRight());
+        int diferencia = Math.abs(alturaIzquierda - alturaDerecha);
+                
+        //verificamos la diferencia de las alturas
+         if(diferencia > 1) {
+             return false;
+         }
+         return verificarBalance(nodo.getLeft()) && verificarBalance(nodo.getRight());
+    }
+     
+    private int obtenerAltura(Nodo nodo) {
+        if(nodo == null){
+            return 0;
+        }
+        int alturaIzquierda = obtenerAltura(nodo.getLeft());
+        int alturaDerecha = obtenerAltura(nodo.getRight());
+        
+        return Math.max(alturaIzquierda, alturaDerecha)+1;
+        }
+    }
+    
+    
+    
+
